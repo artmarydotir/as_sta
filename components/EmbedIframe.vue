dbecbf6407a87e7a369123fd6c58f1779b9c12ba
Art_Mary <artmary.ir@gmail.com>
2020-06-10 13:44:03 +0430

    video in progress.

--- components/EmbedIframe.vue
+++ components/EmbedIframe.vue
@@ -1,25 +1,20 @@
 <template>
   <div class="iframe_container">
-    {{ mutateSrc }}
-    <div v-if="aparat" class="button_child" @click="showAparat">
-      <span>
-        aparat
-      </span>
-    </div>
     <div
       class="iframe_border"
       :style="{ backgroundImage: 'url(\'' + posterUrl + '\')' }"
     >
-      <!-- <div v-if="youtube" class="button_child">
-        {{ youtube }}
+      <div v-show="close" class="button_child" @click="iframeShow">
         <span>
-          youtube
+          Play
         </span>
-      </div> -->
+      </div>
       <client-only>
         <div>
           <iframe
+            v-show="btnShow"
             ref="myVideoEl"
+            class="lazyload"
             :src="isValidUrl"
             allowFullScreen="true"
             webkitallowfullscreen="true"
@@ -34,11 +29,6 @@
 <script>
 export default {
   name: 'IframeComponent',
-  // data() {
-  //   return {
-  //     url: 'https://www.aparat.com/video/video/embed/videohash/wrvNK/vt/frame'
-  //   };
-  // },
   props: {
     // eslint-disable-next-line vue/require-prop-types
     aparat: {
@@ -56,6 +46,9 @@ export default {
   },
   data() {
     return {
+      i: false,
+      btnShow: false,
+      close: true,
       mutateSrc: this.aparat
     };
   },
@@ -84,49 +77,42 @@ export default {
       const srcif = aaa.attributes.src.nodeValue;
       console.log(srcif);
       this.mutateSrc = `${srcif}?autoplay=1`;
+      this.i = true;
       console.log(this.mutateSrc);
+    },
+    iframeShow() {
+      this.btnShow = true;
+      this.close = false;
     }
   }
 };
 </script>
 <style lang="scss" scoped>
-.h_iframe-aparat_embed_frame {
-  position: relative;
-}
-.h_iframe-aparat_embed_frame .ratio {
-  display: block;
-  width: 100%;
-  height: auto;
-}
-.h_iframe-aparat_embed_frame iframe {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
 .iframe_container {
   margin: 20px 0;
   display: flex;
   justify-content: center;
   .iframe_border {
-    // display: flex;
-    // justify-content: space-evenly;
-    // align-items: center;
     background-size: cover;
     background-repeat: no-repeat;
     background-size: cover;
     background-blend-mode: saturation;
     border: 10px solid rgb(230, 101, 101);
-    width: 70%;
-    height: 420px;
+    width: 60%;
+    height: 460px;
   }
 }
 .button_child {
+  padding-top: 15%;
   color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   span {
-    width: 30px;
-    height: 30px;
+    width: 120px;
+    height: 60px;
+    text-align: center;
+    display: inline-block;
     background-color: rgb(204, 152, 84);
     padding: 12px;
     color: #fff;
@@ -135,7 +121,7 @@ export default {
 }
 iframe {
   width: 100%;
-  height: 400px;
+  height: 440px;
   border: none;
 }
 </style>
